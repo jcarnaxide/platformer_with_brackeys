59302e266801d99505c9c22a95c11b03ee35b39a
Jesse Carnaxide <jcarnaxide@gmail.com>
2024-08-05 21:54:34 -0400

    setup animated bottle for use as powerup later

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 @onready var timer = $Timer
+@onready var die_sound = $DieSound
 
 func _on_body_entered(body):
 	print("You died!")
+	die_sound.play()
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
